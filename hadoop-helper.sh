6693e9577f912bf131673f9a4f70f2e47e35013c
Shindo Wu <cn.shindo@gmail.com>
2018-03-09 01:03:21 -0800

    hadoop helper

---
+++ hadoop-helper.sh
@@ -0,0 +1,6 @@
+hdfs dfs -rm -r -f /user/rwu034/index/input/*
+hdfs dfs -rm -r -f /user/rwu034/index/output
+HADOOP_CLIENT_OPTS="-Xmx16g" hadoop jar /extra/rwu034/cs242/cs242.jar mapreduce /user/rwu034/index/input /user/rwu034/index/output > output.log 2>&1
+hdfs dfs -ls /user/rwu034/index/output/
+hdfs dfs -tail /user/rwu034/index/output/part-r-00000
+hdfs dfs -get /user/rwu034/index/output/part-r-00000 index.json
