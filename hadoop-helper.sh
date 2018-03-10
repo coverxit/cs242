7c270ce7b4840e79e6bda77526e22121c501c4c5
Shindo <cn.shindo@gmail.com>
2018-03-09 17:50:58 -0800

    update hadoop helper

--- hadoop-helper.sh
+++ hadoop-helper.sh
@@ -4,3 +4,4 @@ HADOOP_CLIENT_OPTS="-Xmx16g" hadoop jar /extra/rwu034/cs242/cs242.jar mapreduce 
 hdfs dfs -ls /user/rwu034/index/output/
 hdfs dfs -tail /user/rwu034/index/output/part-r-00000
 hdfs dfs -get /user/rwu034/index/output/part-r-00000 index.json
+HADOOP_CLIENT_OPTS="-Xmx16g" hadoop jar cs242.jar mapreduce /index/input /index/output > output.log 2>&1
