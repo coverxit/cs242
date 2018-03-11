9edb3615488080be8e09b122887325365faa21ac
Shindo Wu <cn.shindo@gmail.com>
2018-03-10 17:47:20 -0800

    import 3 field separately

--- src/edu/ucr/cs242/mixer/importer/DocumentLengthImportThread.java
+++ src/edu/ucr/cs242/mixer/importer/DocumentLengthImportThread.java
@@ -25,6 +25,16 @@ public class DocumentLengthImportThread extends Thread {
         this.jsonOutputPath = jsonOutputPath;
     }
 
+    private int putLength(JSONObject json, int fieldId, String fieldName) {
+        int length = new StringTokenizer(json.getString(fieldName)).countTokens();
+
+        // <docId, length>
+        database.put(JniDBFactory.bytes("__docLength_" + json.getInt("id") + "_" + fieldId),
+                JniDBFactory.bytes(String.valueOf(length)));
+
+        return length;
+    }
+
     @Override
     public void run() {
         LocalDateTime startAt = LocalDateTime.now();
@@ -35,19 +45,18 @@ public class DocumentLengthImportThread extends Thread {
         try (BufferedReader dataReader = new BufferedReader(
                 new FileReader(Paths.get(jsonOutputPath, "data.json").toString()))) {
 
-            BigInteger totalDocLength = BigInteger.ZERO;
+            // 0 - title, 1 - content, 2 - categories
+            BigInteger[] totalDocLength = { BigInteger.ZERO, BigInteger.ZERO, BigInteger.ZERO };
 
             String dataLine;
             while ((dataLine = dataReader.readLine()) != null) {
                 try {
                     JSONObject dataJson = new JSONObject(dataLine);
-                    int length = new StringTokenizer(dataJson.getString("content")).countTokens();
 
-                    // <docId, length>
-                    database.put(JniDBFactory.bytes("__docLength_" + dataJson.getInt("id")),
-                            JniDBFactory.bytes(String.valueOf(length)));
+                    totalDocLength[0] = totalDocLength[0].add(BigInteger.valueOf(putLength(dataJson, 0, "title")));
+                    totalDocLength[1] = totalDocLength[1].add(BigInteger.valueOf(putLength(dataJson, 1, "content")));
+                    totalDocLength[2] = totalDocLength[2].add(BigInteger.valueOf(putLength(dataJson, 2, "categories")));
 
-                    totalDocLength = totalDocLength.add(BigInteger.valueOf(length));
                     ++indexedCount;
                     if (indexedCount % 1000 == 0) {
                         System.out.format("DocumentLengthImportThread has imported %d pages. Elapsed time: %s.%n",
@@ -59,14 +68,16 @@ public class DocumentLengthImportThread extends Thread {
                 }
             }
 
-            String averageDocLength = totalDocLength.divide(BigInteger.valueOf(indexedCount)).toString();
-            database.put(JniDBFactory.bytes("__avgDocLength"), JniDBFactory.bytes(averageDocLength));
-            database.put(JniDBFactory.bytes("__docCount"), JniDBFactory.bytes(String.valueOf(indexedCount)));
+            for (int i = 0; i < totalDocLength.length; i++) {
+                String averageDocLength = totalDocLength[i].divide(BigInteger.valueOf(indexedCount)).toString();
+                database.put(JniDBFactory.bytes("__avgDocLength_" + i), JniDBFactory.bytes(averageDocLength));
+                System.out.println("Summary: Average document length for field " + i + " is " + averageDocLength +
+                        " (total: " + totalDocLength[i].toString() + ").");
+            }
 
+            database.put(JniDBFactory.bytes("__docCount"), JniDBFactory.bytes(String.valueOf(indexedCount)));
             System.out.format("Summary: DocumentLengthImportThread has imported %d pages. Elapsed time: %s.%n",
                     indexedCount, Utility.elapsedTime(startAt, LocalDateTime.now()));
-            System.out.println("Summary: Total document length is " + totalDocLength.toString() +
-                    ", average document length is " + averageDocLength + ".");
         } catch (FileNotFoundException e) {
             System.out.println("DocumentLengthImportThread throws a FileNotFoundException.");
             e.printStackTrace();
